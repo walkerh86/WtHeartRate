053c2fc6cf98f29b2e406b37699f15ebd59c11a3
hcj <hcj@twd>
2017-04-11 20:05:14 +0800

    init commit

---
+++ src/com/wt/heartrate/MainActivity.java
@@ -0,0 +1,15 @@
+package com.wt.heartrate;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.heart_rate_monitor_main);
+	}
+}
